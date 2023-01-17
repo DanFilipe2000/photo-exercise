08e9098001779e049c65bd3ff19a42b0d3a05db2
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-16 22:41:44 -0300

    try

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
     public function store(Request $request) {
-        Storage::put('file.jpg', $request->photo);
+        $request->photo->storeAs('/public', 'file.jpg');
     }
 }
