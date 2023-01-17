b07d18e0199b5c74e41d180cd5e987dccc92a88e
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-16 22:20:30 -0300

    first commit

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    public function store(Request $request) {
+        $request->photo->store('public/file.jpg');
+    }
+}
